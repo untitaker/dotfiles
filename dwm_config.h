7e040d1f7495981ddd27a1e7dd93750f555f990b
Markus Unterwaditzer <markus@unterwaditzer.net>
2016-10-09 15:39:46 +0200

    Add hotkeys for multi-monitor setup

--- dwm_config.h
+++ dwm_config.h
@@ -89,10 +89,10 @@ static Key keys[] = {
 	{ MODKEY,                       XK_t,      togglefloating, {0} },
 	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
 	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
-	//{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
-	//{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
-	//{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
-	//{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
+	{ MODKEY,                       XK_m,      focusmon,       {.i = -1 } },
+	{ MODKEY,                       XK_n,      focusmon,       {.i = +1 } },
+	{ MODKEY|ShiftMask,             XK_m,      tagmon,         {.i = -1 } },
+	{ MODKEY|ShiftMask,             XK_n,      tagmon,         {.i = +1 } },
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
